d50db8a46fc9f16d739f317d628e1cec0e01cbb1
Kuba Jaroszewski <jakub.jaroszewski@gmail.com>
2023-04-03 17:35:29 +0200

    Add simple helper script for running in GCE

---
+++ scripts/run-gce.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PUBLIC_IP=$(curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
+
+export AMBIENT_PROXY_PUBLIC_HOST_NAME=$PUBLIC_IP
+export AMBIENT_PROXY_BIND_ADDRESS=0.0.0.0
+
+cargo run --release --bin ambient_proxy
